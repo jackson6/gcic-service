49c83e3c606387088dce1dc70c68a977345dbaa4
odane <odane@ctrl-it.com.jm>
2018-11-20 13:36:37 -0500

    chat service

--- service-client/client/client.go
+++ service-client/client/client.go
@@ -4,6 +4,7 @@ import (
 	microclient "github.com/micro/go-micro/client"
 	userService "github.com/jackson6/gcic-service/user-service/proto/user"
 	emailService "github.com/jackson6/gcic-service/email-service/proto/email"
+	chatService "github.com/jackson6/gcic-service/chat-service/proto/chat"
 	partnerService "github.com/jackson6/gcic-service/partner-service/proto/partner"
 	paymentService "github.com/jackson6/gcic-service/payment-service/proto/payment"
 	planService "github.com/jackson6/gcic-service/plan-service/proto/plan"
@@ -14,6 +15,7 @@ import (
 // app has router and firebase instances
 type Client struct {
 	User userService.UserServiceClient
+	Chat chatService.ChatServiceClient
 	Email emailService.EmailServiceClient
 	Partner partnerService.PartnerServiceClient
 	Payment paymentService.PaymentServiceClient
@@ -24,6 +26,7 @@ type Client struct {
 
 func (c *Client) NewClient() *Client {
 	c.User = userService.NewUserServiceClient("user", microclient.DefaultClient)
+	c.Chat = chatService.NewChatServiceClient("chat", microclient.DefaultClient)
 	c.Email = emailService.NewEmailServiceClient("email", microclient.DefaultClient)
 	c.Partner = partnerService.NewPartnerServiceClient("partner", microclient.DefaultClient)
 	c.Payment = paymentService.NewPaymentServiceClient("payment", microclient.DefaultClient)
